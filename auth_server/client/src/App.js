7f8ab133df333fdddbc3e5750c1d1b20c80c0440
andrewdkim47 <andrewdkim47@gmail.com>
2020-03-17 17:00:53 -0400

    adding auth_server

---
+++ auth_server/client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <a href='http://localhost:8888'> Login to Spotify </a>
+      </div>
+    )
+  }
+}
+
+export default App;
